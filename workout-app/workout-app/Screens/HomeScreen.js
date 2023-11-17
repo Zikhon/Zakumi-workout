8e6813bd76e06821fe87f8035597f2771d73f726
Zikhona Mdingazwe <zikhonamdingazwe@gmail.com>
2023-11-17 09:46:07 +0200

    making a login page

--- workout-app/workout-app/Screens/HomeScreen.js
+++ workout-app/workout-app/Screens/HomeScreen.js
@@ -1,22 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, ImageBackground } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, ImageBackground, ActivityIndicator } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Simulate a delay to represent loading (you can replace this with your actual loading logic)
+  useEffect(() => {
+    const loadingTimer = setTimeout(() => {
+      setIsLoading(false);
+      navigation.navigate('Login');
+    }, 2000); // Adjust the delay time as needed
+
+    return () => clearTimeout(loadingTimer); // Cleanup on component unmount
+  }, [navigation]);
 
   return (
-    <SafeAreaView style={styles.container} >
-      <ImageBackground onPress={() => navigation.navigate('Login')}
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
         source={require('../assets/Image/gym5.jpg')}
         style={styles.backgroundImage}
       >
-      </ImageBackground>
-        <View>
-        <Text style={styles.text}>WOMEN POWER</Text>
+        <View style={styles.overlay}>
+          {/* Your content goes here */}
+          <Text style={styles.text}>WOMEN POWER</Text>
           <Text style={styles.text2}>You Can Do This!!!</Text>
+
+          {/* Loading indicator */}
+          {isLoading && (
+            <ActivityIndicator size="large" color="#ED1DBF" style={styles.loadingIndicator} />
+          )}
         </View>
-      
+      </ImageBackground>
     </SafeAreaView>
   );
 };
@@ -24,23 +40,28 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // color: ,
   },
   backgroundImage: {
-    // flex: 1,
+    flex: 1,
     resizeMode: 'cover',
-    height: 100, 
-    width: 100,
-
+  },
+  overlay: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingBottom: 100,
   },
   text: {
     fontSize: 24,
-    color: '#ED1DBF', 
+    color: '#ED1DBF',
     margin: 20,
   },
   text2: {
     fontSize: 15,
-    color: '#ED1DBF', 
+    color: '#ED1DBF',
+  },
+  loadingIndicator: {
+    marginTop: 20,
   },
 });
 
--- workout-app/workout-app/Screens/Loginpage.js
+++ workout-app/workout-app/Screens/Loginpage.js
@@ -1,21 +1,74 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons'; // Assuming you have FontAwesome icons installed
 
-const Loginpage = () => {
+const LoginPage = () => {
   return (
-    <View >
-      <Text>Loginpage</Text>
+    <View style={styles.container}>
+      {/* Icon at the top center */}
+      <FontAwesome name="user-circle" size={50} color="#ED1DBF" style={styles.icon} />
+
+      {/* "Women Power" text in the middle */}
+      <Text style={styles.text}>Women Power</Text>
+
+      {/* Sign In and Sign Up buttons */}
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Sign In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Text "or continue with" */}
+      <Text style={styles.orText}>or continue with</Text>
+
+      {/* Icons for Google, Facebook, and Email */}
+      <View style={styles.iconsContainer}>
+        <FontAwesome name="google" size={30} color="#ED1DBF" style={styles.icon} />
+        <FontAwesome name="facebook" size={30} color="#ED1DBF" style={styles.icon} />
+        <FontAwesome name="envelope" size={30} color="#ED1DBF" style={styles.icon} />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Loginpage
-
-const styles = StyleSheet.create({ 
-  Text:{
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
- 
+  icon: {
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 24,
+    color: '#ED1DBF',
+    marginBottom: 20,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#ED1DBF',
+    padding: 10,
+    marginHorizontal: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+  },
+  orText: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  iconsContainer: {
+    flexDirection: 'row',
+  },
+});
 
-})
+export default LoginPage;
