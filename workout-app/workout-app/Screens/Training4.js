084547a3709acdbc0239bd5c589e4f5747c3f91b
zikhonamdingazwe@gmail.com <zikhonamdingazwe@gmail.com>
2024-01-30 12:53:03 +0200

    .

---
+++ workout-app/workout-app/Screens/Training4.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Training4 = () => {
+  return (
+    <View>
+      <Text>Training4</Text>
+    </View>
+  )
+}
+
+export default Training4
