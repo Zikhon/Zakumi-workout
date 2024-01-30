f013b68647eaac34699cd0d569bb97f76e0d3953
zikhonamdingazwe@gmail.com <zikhonamdingazwe@gmail.com>
2024-01-30 12:41:07 +0200

    ..

---
+++ workout-app/workout-app/Screens/Training2.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Training2 = () => {
+  return (
+    <View>
+      <Text>Training2</Text>
+    </View>
+  )
+}
+
+export default Training2
+
+const styles = StyleSheet.create({})
