1d51b6cc0c0eb8c38101c03e985f45de3af01d6d
zikhonamdingazwe@gmail.com <zikhonamdingazwe@gmail.com>
2024-02-02 13:22:23 +0200

    .

--- workout-app/workout-app/Screens/Reports.js
+++ workout-app/workout-app/Screens/Reports.js
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import { Calendar } from 'react-native-calendars';
 
 const Reports = () => {
+  const [selectedDate, setSelectedDate] = useState('');
+
+  const onDayPress = (day) => {
+    // Handle the selected date
+    setSelectedDate(day.dateString);
+  };
+
   return (
     <View>
       <Text>Reports</Text>
+      <Calendar
+        onDayPress={onDayPress}
+        markedDates={{
+          [selectedDate]: {
+            selected: true,
+            selectedColor: 'blue',
+          },
+        }}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Reports
+export default Reports;
